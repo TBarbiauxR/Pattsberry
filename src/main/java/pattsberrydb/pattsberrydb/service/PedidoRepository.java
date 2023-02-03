0136fbabb7ed6815559ae0ffa9055fb89ba18eee
Tannya Barbiaux <88693281+TBarbiauxR@users.noreply.github.com>
2023-02-03 01:06:15 -0600

    Update PedidoRepository.java

--- src/main/java/pattsberrydb/pattsberrydb/service/PedidoRepository.java
+++ src/main/java/pattsberrydb/pattsberrydb/service/PedidoRepository.java
@@ -3,7 +3,9 @@ package pattsberrydb.pattsberrydb.service;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import pattsberrydb.pattsberrydb.model.Pedido;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
 }
