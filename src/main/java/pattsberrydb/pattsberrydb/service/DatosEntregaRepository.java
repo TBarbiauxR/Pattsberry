2122dcea6c6a78dd9205f5bd33feecf428a837fc
Tannya Barbiaux <88693281+TBarbiauxR@users.noreply.github.com>
2023-02-03 01:05:10 -0600

    Update DatosEntregaRepository.java

--- src/main/java/pattsberrydb/pattsberrydb/service/DatosEntregaRepository.java
+++ src/main/java/pattsberrydb/pattsberrydb/service/DatosEntregaRepository.java
@@ -4,8 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import pattsberrydb.pattsberrydb.model.DatosEntrega;
 
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface DatosEntregaRepository extends JpaRepository<DatosEntrega, Long> {
 
 }
