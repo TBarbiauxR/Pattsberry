11b14eecf9295b863e8915cf4273fc57f947fc12
Tannya Barbiaux <88693281+TBarbiauxR@users.noreply.github.com>
2023-02-03 01:06:49 -0600

    Update ProductoRepository.java

--- src/main/java/pattsberrydb/pattsberrydb/service/ProductoRepository.java
+++ src/main/java/pattsberrydb/pattsberrydb/service/ProductoRepository.java
@@ -3,7 +3,9 @@ package pattsberrydb.pattsberrydb.service;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import pattsberrydb.pattsberrydb.model.Producto;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long>{
 	
 
