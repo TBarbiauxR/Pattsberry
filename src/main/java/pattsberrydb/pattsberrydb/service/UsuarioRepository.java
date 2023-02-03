c5bb4397499ed010d86ab51c949c5ffc60f7a4b4
Tannya Barbiaux <88693281+TBarbiauxR@users.noreply.github.com>
2023-02-03 01:07:08 -0600

    Update UsuarioRepository.java

--- src/main/java/pattsberrydb/pattsberrydb/service/UsuarioRepository.java
+++ src/main/java/pattsberrydb/pattsberrydb/service/UsuarioRepository.java
@@ -3,7 +3,9 @@ package pattsberrydb.pattsberrydb.service;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import pattsberrydb.pattsberrydb.model.Usuario;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 	
 }//interface
